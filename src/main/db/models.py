aef86c17a2a16c5af8a688ada408afe38318d237
Guillermo Enrique Sánchez de Abreu <naselliug@gmail.com>
2023-04-29 21:17:03 +0200

    updated: sqlalchemy version

--- src/main/db/models.py
+++ src/main/db/models.py
@@ -1,24 +1,30 @@
-from sqlalchemy import Column, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy.types import FLOAT, INTEGER, TIMESTAMP, VARCHAR
+# -*-coding:utf8-*-
+from datetime import datetime
+from typing import List
 
-Base = declarative_base()
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from sqlalchemy.types import FLOAT, Integer, TIMESTAMP, String
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 class ParkRow(Base):
     __tableschema__ = "public"
     __tablename__ = "parks"
-    name = Column(VARCHAR(50), primary_key=True)
-    timezone = Column(VARCHAR(50))
-    energy_type = Column(VARCHAR(50))
-    energy_readings = relationship("EnergyReadingRow", back_populates="park", cascade="all, delete-orphan")
+    name: Mapped[str] = mapped_column(String(50), primary_key=True)
+    timezone: Mapped[str] = mapped_column(String(50))
+    energy_type: Mapped[str] = mapped_column(String(50))
+    energy_readings: Mapped[List["EnergyReadingRow"]] = relationship("EnergyReadingRow", back_populates="park", cascade="all, delete-orphan")
 
 
 class EnergyReadingRow(Base):
     __tableschema__ = "public"
     __tablename__ = "energy_readings"
-    id = Column(INTEGER, primary_key=True)
-    megawatts = Column(FLOAT)
-    timestamp = Column(TIMESTAMP)
-    park_name = Column(VARCHAR(50), ForeignKey("parks.name"))
-    park = relationship("ParkRow", back_populates="energy_readings")
+    id: Mapped[Integer] = mapped_column(Integer, primary_key=True)
+    megawatts: Mapped[float] = mapped_column(FLOAT)
+    timestamp: Mapped[datetime] = mapped_column(TIMESTAMP)
+    park_name: Mapped[str] = mapped_column(String(50), ForeignKey("parks.name"))
+    park: Mapped["ParkRow"] = relationship("ParkRow", back_populates="energy_readings")
